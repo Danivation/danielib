77709ec04c5911ad82d7e01f354e04c1db0f44fe
Daniel Velasco <divelasco7@gmail.com>
2025-09-27 21:33:44 -0500

    feat: sensors and stuff

--- include/danielib/basics/drivetrain.hpp
+++ include/danielib/basics/drivetrain.hpp
@@ -1,5 +1,8 @@
 #pragma once
 #include "pros/motors.h"
+#include "pros/motor_group.hpp"
+#include "danielib/tracking/odometry.hpp"
+#include "danielib/units/units.hpp"
 
 namespace danielib {
 /**
@@ -7,8 +10,13 @@ namespace danielib {
  */
 class drivetrain {
     public:
-        drivetrain(/* args */);
-    private:
-        /* data */
+        drivetrain(pros::MotorGroup* left_motors, pros::MotorGroup* right_motors, danielib::odometry odom_sensors, float track_width, float wheel_size, float wheel_rpm);
+    protected:
+        pros::MotorGroup* left_motors;
+        pros::MotorGroup* right_motors;
+        danielib::odometry odom_sensors;
+        float track_width;
+        float wheel_size;
+        float wheel_rpm;
 };
 } // namespace danielib
--- include/danielib/tracking/odometry.hpp
+++ include/danielib/tracking/odometry.hpp
@@ -1,14 +1,28 @@
 #pragma once
 #include "pros/rotation.hpp"
+#include "pros/imu.hpp"
 
 namespace danielib {
+class tracker_wheel {
+    public:
+        tracker_wheel(pros::Rotation* sensor, float wheel_diameter, float offset);
+    protected:
+        pros::Rotation* sensor;
+        float wheel_diameter;
+        float offset;
+};
+
+class inertial {
+    public:
+        inertial(pros::Imu* sensor, float scale);
+        inertial(pros::Imu* sensor_1, pros::Imu* sensor_2, float scale_1, float scale_2);
+};
+
 /**
  * @brief Class for adding odometry to a drivetrain
  */
 class odometry {
     public:
-        // whatever
-    private:
-        // something
+        odometry(tracker_wheel* vertical_tracker, tracker_wheel* horizontal_tracker, inertial imu);
 };
 } // namespace danielib
